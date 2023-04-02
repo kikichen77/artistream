ef78a7cab8e145af79b744a72ce8a0f1c031858b
Qiqi Chen <77kikichan@gmail.com>
2023-04-02 18:34:36 +1200

    landing page

---
+++ src/LandingPage.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './landingPage.css';
+import { Settings, AddCircle } from '@mui/icons-material';
+
+const LandingPage = () => {
+    // const [roomCode, joinRoom] = useState();
+    // const [username, createRoom] = useState();
+
+    return (
+        <div className="landingPage">
+            <form action="" method="POST">
+                {/* Set an image space */}
+                <div className="imageSpaceContainer">
+                    <div className="imageSpaceDiv">
+                        <img src="vite.svg" alt="" className="userImage"/>
+                    </div>
+                    <span className="addCircleIconContainer">
+                        <AddCircle className="addCircleIcon" />
+                    </span>
+                </div>
+
+                {/* Enter username to display on the main page*/}
+                <p className="usernameText">Username</p>
+                <input type="text" id="username" placeholder="Enter username" className="usernameInput"/><br/>
+        
+                {/* Enter a room code and join a room */}
+                <input type="text" id="roomCode" placeholder="Enter room code" className="roomCodeInput"/>
+                <button className="joinRoomButton" onClick={() => joinRoom(roomCode)}>Join room</button><br/>
+
+                {/* Create a new room */}
+                <button className="createRoomButton" onClick={() => createRoom(username)}>Create Room </button>
+            </form>            
+            <Settings className="settingsIcon" />
+        </div>
+    );
+}
+
+
+
+export default LandingPage;
