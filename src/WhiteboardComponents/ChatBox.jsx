66b94a35b0fa89a66fd05b1951e0eaf57d7797f0
dhe077 <69486294+dhe077@users.noreply.github.com>
2023-04-13 00:06:24 +1200

    Empty Components Added
    - empty Header component
    - empty ConnectedUsers component
    - empty ChatBox component
    - folder for all whiteboard components
    - folder for all whiteboard page styles

---
+++ src/WhiteboardComponents/ChatBox.jsx
@@ -0,0 +1,8 @@
+
+export default function ChatBox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/WhiteboardComponents/ConnectedUsers.jsx
@@ -0,0 +1,8 @@
+
+export default function ConnectedUsers() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
