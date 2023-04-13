dea1e8b44a8ce382d5b812d3493828f864551535
dhe077 <69486294+dhe077@users.noreply.github.com>
2023-04-13 18:07:59 +1200

    Initial Chat Box Implementation
    - chat box added

--- src/WhiteboardComponents/ChatBox.jsx
+++ src/WhiteboardComponents/ChatBox.jsx
@@ -1,8 +1,14 @@
+import styles from "./WhiteboardStyles/ChatBoxStyles.module.css"
+import TextField from '@mui/material/TextField';
 
 export default function ChatBox() {
     return (
-        <div>
-            <p>Chat Box</p>
+        <div className={styles.chatboxBox}>
+            
+            <div className={styles.chatDisplay}>
+                <p className={styles.chatboxText}>Chat Box</p>
+            </div>
+            <TextField className={styles.textField} label="Enter text here..." variant="filled" size="small"/>
         </div>
     )
 }
